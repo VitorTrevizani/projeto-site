36f35845d70c5932bd74bfb229f4dfa29ed9c702
VitorTrevizani <vitoralextrevizani6@gmail.com>
2024-10-10 13:56:56 -0300

    Create meupython.py

---
+++ estilos/meupython.py
@@ -0,0 +1,31 @@
+
+import time
+import pyautogui
+
+pyautogui.PAUSE = 0.5
+pyautogui.press("win")
+pyautogui.press("enter")
+pyautogui.write("chrome")
+pyautogui.press("enter")
+time.sleep(3)
+pyautogui.click(x=491, y=583)
+time.sleep(3)
+pyautogui.click(x=912, y=623)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# time.sleep(5)
+# print(pyautogui.position())
+
+# pyautogui.scroll(200)
